c4054e6ea50da240a47121559af6cb615e917407
Kaylea Nelson <kaylea.nelson@yale.edu>
2017-03-14 18:12:34 -0400

    first pass at fully automated batch, segment only

---
+++ batch_automorph.py
@@ -0,0 +1,69 @@
+import os
+import list_dirs
+import write_settings
+import build_taskfile
+
+function_list = ['segment_sample', 'segment_final', 'focus', '2dmorph', '3dmorph']
+
+
+def load_settings(function):
+
+    filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'automorph.cfg')
+
+    if not os.path.exists(filename):
+
+        parser = ConfigParser.SafeConfigParser()
+        parser.optionxform = str  # preserve case
+        parser.read(filename)
+
+    settings = {}
+
+    for setting in parser.options('global'):
+        settings[setting] = str(parser.get('global', setting))
+
+    for setting in parser.options('metadata'):
+        settings[setting] = str(parser.get('global', setting))
+
+    if 'segment' in function:
+        section = 'segment'
+        settings['mode'] = function.strip('segment_')
+    else:
+        section = function
+
+    for setting in parser.options(section):
+        settings[setting] = str(parser.get(section, setting))
+
+    return settings
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='')
+
+    parser.add_argument('function', help='specify AutoMorph function: %s' % ', '.join(function_list))
+    args = parser.parse_args()
+
+    if args.function not in function_list:
+        print 'Error: unknown function specified. Choose from: %s' % ', '.join(function_list)
+        sys.exit(1)
+
+    settings = load_settings(function)
+
+    if not os.path.exists('dirs_stacks.txt'):
+        list_dirs.list_dirs(raw_images_root, output_root)
+
+    if 'segment' in function:
+        write_settings.segment_settings(settings)
+        build_taskfile.build_taskfile('segment')
+
+    elif '2dmorph' in function:
+        write_settings.twodmorph_settings(settings)
+        build_taskfile.build_taskfile('2dmorph')
+
+    elif '3dmorph' in function:
+        write_settings.threedmorph_settings(settings)
+        build_taskfile.build_taskfile('3dmorph')
+
+
+
+
---
+++ write_settings.py
@@ -0,0 +1,71 @@
+import os
+
+
+def write_settings_file(settings_text, function):
+
+        settings_output = output_root+os.sep+subdirectory
+        print settings_output
+        if not os.path.exists(settings_output):
+            os.makedirs(settings_output)
+
+        f = open(settings_output+os.sep+function+'_settings.txt', 'w')
+
+        for setting in settings_text:
+            f.write(setting+'\n')
+
+        f.close()
+
+
+def segment_settings(settings):
+
+    if settings['mode'] == "final":
+        l = open('dirs_stacks.csv', 'r')
+
+    elif settings['mode'] == "sample":
+        threshold = settings['threshold_range']
+        l = open('dirs_stacks.txt', 'r')
+
+    else:
+        sys.exit("unknown mode")
+
+    for line in l:
+
+        line = line.strip('\n')
+
+        if mode == "final":
+            input_directory, threshold = line.split(',')
+
+        elif mode == "sample":
+            input_directory = line
+
+        subdirectory = os.path.basename(input_directory)
+        source = os.path.basename(input_directory).split('_')[0]
+
+        settings_text = []
+        settings_text.append("[settings]")
+        settings_text.append("directory = "+settings['raw_stacks_root'])
+        settings_text.append("output = "+settings['output_root'])
+        settings_text.append("input_ext = "+settings['original_ext'])
+        settings_text.append("output_ext = tif")
+
+        settings_text.append("minimum_size = "+settings['minimum_size'])
+        settings_text.append("maximum_size = "+settings['maximum_size'])
+
+        settings_text.append("threshold = "+threshold.strip())
+
+        settings_text.append("pixel_size_y = "+settings['pixel_size_y'])
+        settings_text.append("pixel_size_x = "+settings['pixel_size_x'])
+        settings_text.append("unit = "+settings['unit'])
+        settings_text.append("mode = "+setttings['mode'])
+        settings_text.append("skip_last_plane = "+settings['skip_last_plane'])
+        settings_text.append("scale_bar_length = "+settings['scale_bar_length'])
+
+        settings_text.append("age = "+settings['age'])
+        settings_text.append("source = "+source)
+        settings_text.append("author = "+settings['author'])
+        settings_text.append("location = "+settings['location'])
+        settings_text.append("catalog_prefix = "+settings['catalog_prefix'])
+
+        write_settings_file(settings_text, 'segment')
+
+
