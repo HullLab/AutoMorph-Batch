756b9d1a59771999ea875fa0a65a4a080304725d
Kaylea Nelson <kaylea.nelson@yale.edu>
2017-03-24 16:19:58 -0400

    added first pass at 2dmorph basic batching

--- list_dirs.py
+++ list_dirs.py
@@ -1,22 +1,26 @@
-from os import walk
+import os
+import argparse
 import shutil
 
 # location of original stacks to be segmented
-raw_images_root = ""
+raw_images_root = "/gpfs/scratch60/geo/hull/data/porosity"
 # location of output from segment
-output_root = ""
+output_root = "/home/fas/hull/jeb247/project/porosity_segmented"
 
 
-def list_dirs(raw_images_root, output_root):
+def list_dirs_originals(raw_images_root, output_root):
 
     f = open('dirs_stacks.txt', 'w')
-    for root, dirs, files in walk(raw_images_root):
+    for root, dirs, files in os.walk(raw_images_root):
         if len(dirs) == 0:
             f.write(root+'\n')
     f.close()
 
+
+def list_dirs_segmented(raw_images_root, output_root):
+
     f = open('dirs_segmented.txt', 'w')
-    for root, dirs, files in walk(output_root):
+    for root, dirs, files in os.walk(output_root):
         if len(dirs) == 0 or 'final' in dirs or 'sample' in dirs:
             if 'final' not in root and 'sample' not in root:
                 f.write(root+'\n')
@@ -26,6 +30,31 @@ def list_dirs(raw_images_root, output_root):
     shutil.copy('dirs_stacks.txt', 'dirs_stacks.csv')
 
 
+def list_dirs_focused(output_root):
+
+    f = open('dirs_focused.txt', 'w')
+    for root, dirs, files in os.walk(output_root):
+        if os.path.basename(root) == 'final':
+            if 'focused_unlabeled' in dirs:
+                f.write(os.path.dirname(root)+'\n')
+            dirs[:] = []
+
+    f.close()
+
+
 if __name__ == "__main__":
 
-    list_dirs(raw_images_root, output_root)
+    parser = argparse.ArgumentParser(description='')
+
+    parser.add_argument('phase', help='specify: presegment, segmented or focused')
+    args = parser.parse_args()
+
+    if args.phase == 'presegment':
+        list_dirs_originals(raw_images_root)
+        list_dirs_segmented(output_root)
+    elif args.phase == 'segmented':
+        list_dirs_segment(output_root)
+    elif args.phase == 'focused':
+        list_dirs_focused(output_root)
+    else:
+        sys.exit('Unknown phase. Specify presegment, segmented or focused')
---
+++ write_2dmorph_settings.py
@@ -0,0 +1,69 @@
+import os
+
+output_directory = '/home/fas/hull/jeb247/project'
+twodmorph_run_name = '2dmorph_test'
+
+output_directory += os.sep+twodmorph_run_name
+
+install_dir = '/home/geo/hull/ph269/software/AutoMorph'
+
+l = open('dirs_focused.txt','r')
+
+out_dir_list = []
+
+for line in l:
+
+    directory = line.strip()
+    print directory
+    sampleID = os.path.basename(directory)
+
+    sample_out_directory = output_directory+os.sep+sampleID
+    focused_directory = directory+'/final/focused_unlabeled'
+
+    if os.path.exists(focused_directory):
+
+        out_dir_list.append(sample_out_directory)
+
+        if not os.path.exists(sample_out_directory):
+                os.makedirs(sample_out_directory)
+
+        settings_text = []
+        settings_text.append("directory = "+directory)
+        settings_text.append ("output_dir = "+sample_out_directory)
+        settings_text.append("image_extension = .tif")
+
+        settings_text.append("sampleID = "+sampleID)
+
+        settings_text.append("output_filename = []")
+        settings_text.append("microns_per_pixel_Y = 1")
+        settings_text.append("microns_per_pixel_X = 1")
+        settings_text.append("get_coordinates = []")
+        settings_text.append("save_intermediates = []")
+        settings_text.append("intensity_range_in = []")
+        settings_text.append("intensity_range_out = []")
+        settings_text.append("gamma = []")
+        settings_text.append("threshold_adjustment = []")
+        settings_text.append("smoothing_sigma = []")
+        settings_text.append("noise_limit = []")
+        settings_text.append("write_csv = []")
+        settings_text.append("downsample = []")
+        settings_text.append("num_points = []")
+        settings_text.append("draw_ar = []")
+        #print settings_text
+
+       f = open(sample_out_directory+os.sep+'2dmorph_settings.txt', 'w')
+
+       for setting in settings_text:
+           f.write(setting+'\n')
+
+       f.close()
+    else:
+       print "no focused_unlabeled directory: "+directory
+
+# Write directory list
+f = open('dirs_'+twodmorph_run_name+'.txt', 'w')
+
+for directory in out_dir_list:
+    f.write(directory+'\n')
+
+f.close()
